40e1b62bb8cc115a9499bc291af5757d085658bb
Arran Woodruff <arran@freshworks.io>
2022-01-27 16:40:23 -0700

    remove bucket logs

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -42,20 +42,10 @@ resource "aws_cloudfront_function" "request" {
   code    = file("${path.module}/cloudfront/request.js")
 }
 
-
-resource "aws_s3_bucket" "app_logs" {
-  bucket = "${local.app_name}-logs"
-  acl    = "log-delivery-write"
-}
-
 resource "aws_cloudfront_distribution" "app" {
   comment = local.app_name
 
   aliases = local.has_domain ? [var.domain] : []
-
-  logging_config {
-    bucket = aws_s3_bucket.app_logs.bucket_domain_name
-  }
 
   origin {
     domain_name = aws_s3_bucket.app.bucket_regional_domain_name
