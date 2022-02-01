05361fb48e53e52af01735a4a09cb87a8ea35f8c
Will S <will@freshworks.io>
2022-02-01 15:31:55 -0800

    Missed the security headers from the nocache response

--- terraform/cloudfront/response-nocache.js
+++ terraform/cloudfront/response-nocache.js
@@ -4,6 +4,14 @@ function handler(event) {
 
   // Set HTTP security headers
   // Since JavaScript doesn't allow for hyphens in variable names, we use the dict["key"] notation
+  headers['strict-transport-security'] = { value: 'max-age=63072000; includeSubdomains; preload' };
+  headers['content-security-policy'] = {
+    value:
+      "default-src 'self' s3.ca-central-1.amazonaws.com; frame-ancestors 'self'; form-action 'self';",
+  };
+  headers['x-content-type-options'] = { value: 'nosniff' };
+  headers['x-frame-options'] = { value: 'DENY' };
+  headers['x-xss-protection'] = { value: '1; mode=block' };
   headers['cache-control'] = { value: 'no-cache' }
 
   // Return the response to viewers
