2a85d8a5698a5f0b7acb45e53f92163aa154a80e
Arran Woodruff <arran@freshworks.io>
2022-02-11 09:25:17 -0700

    2: Update service bc link

--- components/ServiceBCLink.tsx
+++ components/ServiceBCLink.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link';
 
 const serviceBCNumber = '1-888-268-4319';
-const serviceBCLabel = '1-888-COVID19';
 
 export const ServiceBCLink: React.FC = () => {
   return (
     <Link href={`tel:${serviceBCNumber}`}>
-      <a className='font-bold'>{serviceBCLabel}</a>
+      <a className='font-bold'>{serviceBCNumber}</a>
     </Link>
   );
 };
