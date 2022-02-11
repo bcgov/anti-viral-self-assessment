3239b8d0f331b27ce7c12571c38803f8838e32fa
Arran Woodruff <arran@freshworks.io>
2022-02-11 09:52:39 -0700

    11: Q6 Yes response text update

--- components/EndOfJourney.tsx
+++ components/EndOfJourney.tsx
@@ -200,12 +200,12 @@ const TooManyDays: React.FC = () => (
 
 const AntiviralBenefit: React.FC = () => (
   <EndOfJourneyContainer>
-    <EndOfJourneyTitle>You may benefit from COVID19 treatment.</EndOfJourneyTitle>
+    <EndOfJourneyTitle>You may benefit from COVID-19 treatment.</EndOfJourneyTitle>
     <p>
-      Please call Service BC at <ServiceBCLink /> immediately so they can start the assessment
-      process. Tell them you are calling about COVID19 treatment options and that you have completed
-      the online screener. There is a short window of time to deliver these medications, so please
-      call right away.
+      Please call Service BC at <ServiceBCLink /> immediately so they can continue the assessment
+      process. Tell them you are calling about COVID-19 treatment options and that you have
+      completed the online screener. There is a short window of time to deliver these medications,
+      so please call right away.
     </p>
   </EndOfJourneyContainer>
 );
