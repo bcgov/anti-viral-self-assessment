e549b66e07fed1cd503d4fed0ea0344c12b1ab43
Will S <will@freshworks.io>
2022-02-23 12:55:46 -0800

    Update maintenance page copy

--- components/Maintenance.tsx
+++ components/Maintenance.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { InfoBox } from './InfoBox';
+import { ServiceBCLink } from './ServiceBCLink';
 
 export const Maintenance = () => {
   return (
     <div className='flex items-center justify-center h-full'>
       <div className='md:w-1/2'>
-        <InfoBox>This application is currently unavailable. Please check in a later time.</InfoBox>
+        <InfoBox>
+          This page is currently unavailable. 
+          Please contact Service BC at <ServiceBCLink/> if you have questions about COVID-19 treatments.
+        </InfoBox>
       </div>
     </div>
   );
--- components/ServiceBCLink.tsx
+++ components/ServiceBCLink.tsx
@@ -5,7 +5,7 @@ const serviceBCNumber = '1-888-268-4319';
 export const ServiceBCLink: React.FC = () => {
   return (
     <Link href={`tel:${serviceBCNumber}`}>
-      <a className='font-bold'>{serviceBCNumber}</a>
+      <a className='font-bold whitespace-nowrap'>{serviceBCNumber}</a>
     </Link>
   );
 };
