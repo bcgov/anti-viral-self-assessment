526dab7ea5588479446b2e3018b4d29c9389440d
ninosamson <33761012+ninosamson@users.noreply.github.com>
2022-04-08 14:49:02 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       </h1>
 
       <div className='flex flex-col gap-4 mb-4'>
-        <p>This guidance is based on known evidence as of March 23, 2022.</p>
+        <p>This guidance is based on known evidence as of April 12, 2022.</p>
 
         <div>
           <p>
